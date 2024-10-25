4e20e4b4a6ccd3a4be910dd6a2dbe41cc0f4196c
fuz1700 <fuz77it@gmail.com>
2024-10-25 16:02:45 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let ball = prompt("balni yoz");
+
+      
+                                       
+
+    
+    ball = Number(ball);
+    
+    if (ball <= 70) {
+      alert("kirolmading 🎃🎃🎃");
+    } else if (ball > 70 && ball <= 80) {
+      alert("super kontrakt 🎇🎇");
+    } else if (ball > 80 && ball <= 90) {
+      alert("kontrakt ✨✨");
+    } else if (ball > 90) {
+      alert("grant tabriklaymiz 🎉🎉");
+    } else {  
+      alert("error❌❌❌");
+    }
+
+
+
+
+
+
+    
+ 
